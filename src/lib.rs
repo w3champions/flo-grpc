cabeb47f53d8a282871722063cd8d021b896f59d
Flux Xu <fluxxu@gmail.com>
2020-07-11 23:55:10 -0400

    feat: connect service & adjustments.

--- src/lib.rs
+++ src/lib.rs
@@ -1,10 +1,26 @@
 pub use anyhow::{Error, Result};
 pub use tonic::transport::{Channel, Endpoint};
 
+pub mod player {
+  tonic::include_proto!("player");
+}
+
+pub mod game {
+  tonic::include_proto!("game");
+}
+
+pub mod auth {
+  tonic::include_proto!("auth");
+}
+
 pub mod lobby {
   tonic::include_proto!("lobby");
 }
 
 pub mod node {
   tonic::include_proto!("node");
+}
+
+pub mod connect {
+  tonic::include_proto!("connect");
 }
